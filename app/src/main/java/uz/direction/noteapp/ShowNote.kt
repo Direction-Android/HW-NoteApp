ba0c11dab97af97dae99ea5cbb4d02318a56b50a
Aidein <sultanovazamat985@gmail.com>
2022-04-28 20:51:52 +0500

    First push

---
+++ app/src/main/java/uz/direction/noteapp/ShowNote.kt
@@ -0,0 +1,11 @@
+package uz.direction.noteapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowNote : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_note)
+    }
+}
