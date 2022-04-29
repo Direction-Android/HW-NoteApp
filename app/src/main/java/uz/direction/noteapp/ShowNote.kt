aea0800a8c7da36f3d8c09695d66d6ecd023ea02
Aidein <sultanovazamat985@gmail.com>
2022-04-29 17:24:08 +0500

    Homework completed

--- app/src/main/java/uz/direction/noteapp/ShowNote.kt
+++ app/src/main/java/uz/direction/noteapp/ShowNote.kt
@@ -2,10 +2,18 @@ package uz.direction.noteapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import uz.direction.noteapp.databinding.ActivityMainBinding
+import uz.direction.noteapp.databinding.ActivityShowNoteBinding
 
 class ShowNote : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_show_note)
+        val binding = ActivityShowNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.headerTitleNote.text = intent.getStringExtra(MainActivity.MAIN_HEADER_MESSAGE)
+        binding.text.text = intent.getStringExtra(MainActivity.MAIN_TEXT_MESSAGE)
     }
 }
