028c4ed092c9cddabbfbaef535c305e012de8a05
Aidein <sultanovazamat985@gmail.com>
2022-07-04 13:28:35 +0500

    project reset

--- app/src/main/java/uz/direction/noteapp/MainActivity.kt
+++ app/src/main/java/uz/direction/noteapp/MainActivity.kt
@@ -1,48 +1,14 @@
 package uz.direction.noteapp
 
-import android.content.Context
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import uz.direction.noteapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    companion object{
-        const val MAIN_HEADER_MESSAGE = "header"
-        const val MAIN_TEXT_MESSAGE = "text"
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         val binding = ActivityMainBinding.inflate(layoutInflater)
-        val sharedPref = getSharedPreferences(getString(R.string.shared_preference_key),
-            Context.MODE_PRIVATE)
-
         setContentView(binding.root)
-
-        binding.headerTitle.setText(sharedPref.getString(getString(R.string.header_key), ""))
-        binding.textValue.setText(sharedPref.getString(getString(R.string.text_key), ""))
-
-        binding.saveButton.setOnClickListener{
-            val intent = Intent(this, ShowNote::class.java)
-            val header = binding.headerTitle.text.toString()
-            val text = binding.textValue.text.toString()
-
-            if ((header == "") || (text == "")){
-                Toast.makeText(baseContext, "Header or text missing", Toast.LENGTH_SHORT).show()
-            } else{
-                with(sharedPref.edit()){
-                    putString(getString(R.string.header_key), header)
-                    putString(getString(R.string.text_key), text)
-                    apply()
-                }
-
-                intent.putExtra(MAIN_HEADER_MESSAGE, header)
-                intent.putExtra(MAIN_TEXT_MESSAGE, text)
-                startActivity(intent)
-            }
-        }
     }
 }
